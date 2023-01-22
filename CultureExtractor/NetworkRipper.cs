6d0e0d2f861bf452ff0cbdba9c6b998cad504af6
Ville Salonen <ville.salonen@iki.fi>
2023-01-22 16:03:04 +0200

    Refactoring NetworkRipper to publicly expose relevant methods

--- CultureExtractor/NetworkRipper.cs
+++ CultureExtractor/NetworkRipper.cs
@@ -17,111 +17,11 @@ public class NetworkRipper
         _repository = repository;
     }
 
-    public async Task InitializeAsync(string shortName, JobType jobType, BrowserSettings browserSettings, DownloadConditions downloadConditions)
+    public async Task ScrapeScenesAsync(Site site, BrowserSettings browserSettings)
     {
-        var site = await _repository.GetSiteAsync(shortName);
+        ISceneScraper? sceneScraper = GetRipper<ISceneScraper>(site.ShortName);
+        Log.Information($"Culture Extractor, using {sceneScraper.GetType()}");
 
-        switch (jobType)
-        {
-            case JobType.ScrapeScenes:
-                ISceneScraper? siteRipper = GetRipper<ISceneScraper>(shortName);
-                Log.Information($"Culture Extractor, using {siteRipper.GetType()}");
-                await ScrapeScenesAsync(siteRipper, site, browserSettings);
-                break;
-            case JobType.DownloadScenes:
-                ISceneDownloader? sceneDownloader = GetRipper<ISceneDownloader>(shortName);
-                Log.Information($"Culture Extractor, using {sceneDownloader.GetType()}");
-
-                var preferredDownloadQuality = Enum.GetName(downloadConditions.PreferredDownloadQuality);
-
-                var matchingScenes = await _repository._sqliteContext.Scenes
-                    .Include(s => s.Performers)
-                    .Include(s => s.Tags)
-                    .Include(s => s.Site)
-                    .Include(s => s.Downloads)
-                    .OrderBy(s => s.ReleaseDate)
-                    .Where(s => s.SiteId == site.Id)
-                    .Where(s => downloadConditions.DateRange == null || (downloadConditions.DateRange.Start <= s.ReleaseDate && s.ReleaseDate <= downloadConditions.DateRange.End))
-                    .Where(s => downloadConditions.PerformerShortName == null || s.Performers.Any(p => p.ShortName == downloadConditions.PerformerShortName))
-                    .Where(s => !s.Downloads.Any(d => d.DownloadQuality == preferredDownloadQuality))
-                .ToListAsync();
-
-                await DownloadScenesAsync(
-                    sceneDownloader,
-                    matchingScenes,
-                    site,
-                    downloadConditions,
-                    browserSettings);
-                break;
-            case JobType.UpsizeDownloadedScenes:
-                ISceneDownloader? sceneDownloader2 = GetRipper<ISceneDownloader>(shortName);
-                Log.Information($"Culture Extractor, using {sceneDownloader2.GetType()}");
-
-                var preferredDownloadQuality2 = Enum.GetName(downloadConditions.PreferredDownloadQuality);
-
-                var fileNames = new List<string>
-                {
-                    @" - PurgatoryX - 2021-07-09 - My Wife’s Massage Vol 2 E1.mp4",
-                    @"Aidra Fox & Donnie Rock - PurgatoryX - 2019-10-11 - The Last Straw Vol 1 E1.mp4",
-                    @"Angela White & Donnie Rock - PurgatoryX - 2019-11-01 - The Dentist Vol 1 E3.mp4",
-                    @"Anny Aurora & Donnie Rock - PurgatoryX - 2019-12-27 - The Dentist Vol 2 E1.mp4",
-                    @"Autumn Falls & Lena Paul - PurgatoryX - 2019-03-29 - The Therapist Vol 1 E1.mp4",
-                    @"Autumn Falls & Lena Paul - PurgatoryX - 2019-05-10 - The Therapist Vol 1 E2.mp4",
-                    @"Autumn Falls, Lena Paul & Donnie Rock - PurgatoryX - 2019-06-07 - The Therapist Vol 1 E3.mp4",
-                    @"Bella Rolland, Donnie Rock & La Sirena 69 - PurgatoryX - 2020-10-02 - Genie Wishes Vol 2 E2.mp4",
-                    @"Charles Dera, Donnie Rock & April Snow - PurgatoryX - 2018-12-21 - Fantasy Couple E3.mp4",
-                    @"Charles Dera, Donnie Rock & Cassie Cloutier - PurgatoryX - 2019-01-18 - My Wife’s Massage E2.mp4",
-                    @"Charles Dera, Donnie Rock & Jaye Summers - PurgatoryX - 2019-04-19 - My Husband Convinced Me Vol 1 E1.mp4",
-                    @"Charles Dera, Donnie Rock & Sherly Queen - PurgatoryX - 2018-10-26 - My Wife’s Massage E1.mp4",
-                    @"Charles Dera, Donnie Rock, Jaye Summers & Vienna Black - PurgatoryX - 2019-06-14 - My Husband Convinced Me Vol 1 E2.mp4",
-                    @"Charles Dera, Donnie Rock, Jaye Summers & Vienna Black - PurgatoryX - 2019-07-26 - My Husband Convinced Me Vol 1 E3.mp4",
-                    @"Cherie DeVille, Charles Dera & Donnie Rock - PurgatoryX - 2019-03-01 - My Wife’s Massage E3.mp4",
-                    @"Demi Sutra & Donnie Rock - PurgatoryX - 2019-09-20 - The Dentist Vol 1 E2.mp4",
-                    @"Donnie Rock & April Snow - PurgatoryX - 2018-11-23 - Fantasy Couple E1.mp4",
-                    @"Donnie Rock, April Snow & Nadya Nabakova - PurgatoryX - 2018-12-07 - Fantasy Couple E2.mp4",
-                    @"Emily Willis & Aidra Fox - PurgatoryX - 2019-11-08 - The Last Straw Vol 1 E2.mp4",
-                    @"Emily Willis, Aidra Fox & Donnie Rock - PurgatoryX - 2019-12-06 - The Last Straw Vol 1 E3.mp4",
-                    @"Jennifer White & Donnie Rock - PurgatoryX - 2020-08-07 - Genie Wishes Vol 2 E1.mp4",
-                    @"Kendra Spade & Donnie Rock - PurgatoryX - 2019-08-23 - The Dentist Vol 1 E1.mp4",
-                    @"Khloe Kapri & Donnie Rock - PurgatoryX - 2020-02-07 - The Dentist Vol 2 E2.mp4",
-                    @"Krissy Lynn, Donnie Rock, Ramon Nomar & La Sirena 69 - PurgatoryX - 2020-11-27 - Genie Wishes Vol 2 E3.mp4",
-                    @"Lacy Lennon, Gianna Dior & Donnie Rock - PurgatoryX - 2020-03-27 - Let Me Watch Vol 2 E1.mp4",
-                    @"Lacy Lennon, Gianna Dior & Seth Gamble - PurgatoryX - 2020-05-15 - Let Me Watch Vol 2 E2.mp4",
-                    @"Lacy Lennon, Gianna Dior, Seth Gamble & Donnie Rock - PurgatoryX - 2020-07-10 - Let Me Watch Vol 2 E3.mp4",
-                    @"Laney Grey, Codey Steele & Alex Mack - PurgatoryX - 2020-10-30 - My Fiancée's Wishes Vol 1 E2.mp4",
-                    @"Laney Grey, Codey Steele & Stirling Cooper - PurgatoryX - 2020-09-04 - My Fiancée’s Wishes Vol 1 E1.mp4",
-                    @"Laney Grey, Codey Steele, Stirling Cooper & Alex Mack - PurgatoryX - 2020-12-25 - My Fiancée's Wishes Vol 1 E3.mp4",
-                    @"Markus Kage, Rowan Rails & Samantha Creams - PurgatoryX - 2021-09-03 - My Wife's Massage Vol 2 E2.mp4",
-                    @"Maya Bijou, Charles Dera & Bambi Black - PurgatoryX - 2019-05-24 - Let Me Watch Vol 1 E2.mp4",
-                    @"Maya Bijou, Charles Dera, Donnie Rock & Bambi Black - PurgatoryX - 2019-06-21 - Let Me Watch Vol 1 E3.mp4",
-                    @"Maya Bijou, Donnie Rock & Bambi Black - PurgatoryX - 2019-04-26 - Let Me Watch Vol 1 E1.mp4",
-                    @"Vanna Bardot & Donnie Rock - PurgatoryX - 2020-03-06 - The Dentist Vol 2 E3.mp4"
-                };
-
-                var matchingScenes2 = await _repository._sqliteContext.Scenes
-                    .Include(s => s.Performers)
-                    .Include(s => s.Tags)
-                    .Include(s => s.Site)
-                    .Include(s => s.Downloads)
-                    .OrderBy(s => s.ReleaseDate)
-                    .Where(s => s.Downloads.Any(d => fileNames.Contains(d.SavedFilename)))
-                    .Where(s => !s.Downloads.Any(d => d.DownloadQuality == preferredDownloadQuality2))
-                .ToListAsync();
-
-                await DownloadScenesAsync(
-                    sceneDownloader2,
-                    matchingScenes2,
-                    site,
-                    downloadConditions,
-                    browserSettings);
-                break;
-            default:
-                throw new Exception($"Could not find a ripper for job {Enum.GetName(jobType)} with site short name {shortName}");
-        }
-    }
-
-    private async Task ScrapeScenesAsync(ISceneScraper sceneScraper, Site site, BrowserSettings browserSettings)
-    {
         IPage page = await CreatePageAndLoginAsync(sceneScraper, site, browserSettings);
         var totalPages = await sceneScraper.NavigateToScenesAndReturnPageCountAsync(site, page);
 
@@ -187,11 +87,58 @@ public class NetworkRipper
         }
     }
 
-    public async Task DownloadScenesAsync(ISceneDownloader sceneDownloader, IList<SceneEntity> matchingScenes, Site site, DownloadConditions conditions, BrowserSettings browserSettings)
+    public async Task DownloadScenesAsync(Site site, BrowserSettings browserSettings, DownloadConditions downloadConditions)
+    {
+        var preferredDownloadQuality = Enum.GetName(downloadConditions.PreferredDownloadQuality);
+
+        var matchingScenes = await _repository._sqliteContext.Scenes
+            .Include(s => s.Performers)
+            .Include(s => s.Tags)
+            .Include(s => s.Site)
+            .Include(s => s.Downloads)
+            .OrderBy(s => s.ReleaseDate)
+            .Where(s => s.SiteId == site.Id)
+            .Where(s => downloadConditions.DateRange == null || (downloadConditions.DateRange.Start <= s.ReleaseDate && s.ReleaseDate <= downloadConditions.DateRange.End))
+            .Where(s => downloadConditions.PerformerShortName == null || s.Performers.Any(p => p.ShortName == downloadConditions.PerformerShortName))
+            .Where(s => !s.Downloads.Any(d => d.DownloadQuality == preferredDownloadQuality))
+        .ToListAsync();
+
+        await DownloadGivenScenesAsync(
+            site,
+            browserSettings,
+            downloadConditions,
+            matchingScenes);
+    }
+
+    public async Task UpsizeScenesAsync(Site site, BrowserSettings browserSettings, DownloadConditions downloadConditions, IList<string> fileNames)
+    {
+        var preferredDownloadQuality = Enum.GetName(downloadConditions.PreferredDownloadQuality);
+
+        var matchingScenes = await _repository._sqliteContext.Scenes
+            .Include(s => s.Performers)
+            .Include(s => s.Tags)
+            .Include(s => s.Site)
+            .Include(s => s.Downloads)
+            .OrderBy(s => s.ReleaseDate)
+            .Where(s => s.Downloads.Any(d => fileNames.Contains(d.SavedFilename)))
+            .Where(s => !s.Downloads.Any(d => d.DownloadQuality == preferredDownloadQuality))
+        .ToListAsync();
+
+        await DownloadGivenScenesAsync(
+            site,
+            browserSettings,
+            downloadConditions,
+            matchingScenes);
+    }
+
+    private async Task DownloadGivenScenesAsync(Site site, BrowserSettings browserSettings, DownloadConditions downloadConditions, IList<SceneEntity> matchingScenes)
     {
         var matchingScenesStr = string.Join($"{Environment.NewLine}    ", matchingScenes.Select(s => $"{s.Site.Name} - {s.ReleaseDate.ToString("yyyy-MM-dd")} - {s.Name}"));
 
-        Log.Information($"Found {matchingScenes.Count()} scenes:{Environment.NewLine}    {matchingScenesStr}");
+        ISceneDownloader? sceneDownloader = GetRipper<ISceneDownloader>(site.ShortName);
+        Log.Information($"Culture Extractor, using {sceneDownloader.GetType()}");
+
+        Log.Information($"Found {matchingScenes.Count} scenes:{Environment.NewLine}    {matchingScenesStr}");
 
         if (!matchingScenes.Any())
         {
@@ -208,9 +155,9 @@ public class NetworkRipper
         var rippedScenes = 0;
         foreach (var scene in matchingScenes)
         {
-            if (rippedScenes >= conditions.MaxDownloads)
+            if (rippedScenes >= downloadConditions.MaxDownloads)
             {
-                Log.Information($"Maximum scene rip limit of {conditions.MaxDownloads} reached. Stopping...");
+                Log.Information($"Maximum scene rip limit of {downloadConditions.MaxDownloads} reached. Stopping...");
                 break;
             }
             if ((matchingScenes.Count - rippedScenes) % 10 == 0)
@@ -233,13 +180,13 @@ public class NetworkRipper
                         Log.Information($"Retrying {retries + 1} attempt for {scene.Url}");
                     }
 
-                    var download = await sceneDownloader.DownloadSceneAsync(scene, page, rippingPath, conditions);
+                    var download = await sceneDownloader.DownloadSceneAsync(scene, page, rippingPath, downloadConditions);
 
                     _repository._sqliteContext.Downloads.Add(new DownloadEntity()
                     {
                         DownloadedAt = DateTime.Now,
                         DownloadDetails = JsonSerializer.Serialize(download.DownloadDetails),
-                        DownloadQuality = Enum.GetName(conditions.PreferredDownloadQuality),
+                        DownloadQuality = Enum.GetName(downloadConditions.PreferredDownloadQuality),
                         OriginalFilename = download.OriginalFilename,
                         SavedFilename = download.SavedFilename,
 
@@ -277,6 +224,7 @@ public class NetworkRipper
                 }
             }
         }
+
     }
 
     private async Task<IPage> CreatePageAndLoginAsync(ISiteScraper siteScraper, Site site, BrowserSettings browserSettings)
--- CultureExtractor/Program.cs
+++ CultureExtractor/Program.cs
@@ -25,6 +25,9 @@ class PlaywrightExample
             string shortName = args[0];
             var browserSettings = new BrowserSettings(false);
 
+            // var downloadOptions = DownloadConditions.All(PreferredDownloadQuality.Phash);
+            var downloadOptions = DownloadConditions.All(PreferredDownloadQuality.Best) with { PerformerShortName = "401-alexis-crystal" };
+
             var jobType = args[1] switch
             {
                 "scrape-scenes" => JobType.ScrapeScenes,
@@ -33,14 +36,89 @@ class PlaywrightExample
                 _ => throw new NotImplementedException($"Unknown job type {args[1]}")
             };
 
-            var networkRipper = new NetworkRipper(new Repository(new SqliteContext()));
-            await networkRipper.InitializeAsync(
-                args[0],
-                jobType,
-                browserSettings,
-                DownloadConditions.All(PreferredDownloadQuality.Phash));
-                // DownloadConditions.All(PreferredDownloadQuality.Best) with { PerformerShortName = "d3e8c73e/nancy-a" });
-                // DownloadConditions.All(PreferredDownloadQuality.Best));
+            var repository = new Repository(new SqliteContext());
+            var site = await repository.GetSiteAsync(shortName);
+            var networkRipper = new NetworkRipper(repository);
+
+            switch (jobType)
+            {
+                case JobType.ScrapeScenes:
+                    await networkRipper.ScrapeScenesAsync(site, browserSettings);
+                    break;
+                case JobType.DownloadScenes:
+                    await networkRipper.DownloadScenesAsync(site, browserSettings, downloadOptions);
+                    break;
+                case JobType.UpsizeDownloadedScenes:
+                    var fileNames = new List<string>
+                    {
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal & Isabella Chrystin - Czech VR - 2017-04-01 - Where is the map.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal & Isabella Chrystin - Czech VR - 2017-04-03 - Lesbian pirates in action.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal - Czech VR - 2017-03-11 - Baking with Alexis.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal - Czech VR - 2019-06-22 - Kinky Toy.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal, Charlie Red, Jenifer Jane & Stacy Cruz - Czech VR - 2019-04-22 - Easter Race.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal, Charlie Red, Olivia Sparkle & Rika Fane - Czech VR - 2022-12-21 - A Christmas Dream Part 1.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal, Charlie Red, Olivia Sparkle & Rika Fane - Czech VR - 2022-12-23 - A Christmas Dream Part 2.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal, Lexi Dona & Anie Darling - Czech VR - 2017-08-05 - Naughty Bride with Friends.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alexis Crystal, Venera Maxima & Emma Button - Czech VR - 2020-06-13 - Quarantine Rent.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Alissa Foxy, Isabela De Laa & Lilly Bella - Czech VR - 2022-11-07 - Lesbian Threesome.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Amber Deen & Arteya - Czech VR - 2018-09-08 - Cock as a Learning Tool.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Amy Amor - Czech VR - 2021-11-13 - Tail Wagging.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Anna Claire Clouds - Czech VR - 2020-11-11 - Hostel Mix-Up.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Anna de Ville - Czech VR - 2018-08-25 - Sexy Moving.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Anna de Ville - Czech VR - 2021-04-24 - Virtual VR Babe.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Antonia Sainz & Emylia Argan - Czech VR - 2020-06-27 - It Began With VR.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Antonia Sainz & Nessie Blue - Czech VR - 2021-03-20 - Accidental Cheating - Intentional Threesome.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Antonia Sainz - Czech VR - 2022-10-22 - Horny First Date.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Antonia Sainz, Billie Star & Kristy Black - Czech VR - 2021-04-17 - Anal Heaven.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Candice Demellza & Nikki Hill - Czech VR - 2022-01-01 - Call Girl Threesome.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Casey Nice & Jessika Night - Czech VR - 2021-09-11 - Fun in the Pool.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Casey Nice - Czech VR - 2021-03-13 - Stay and Watch!.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Casey Nice - Czech VR - 2022-10-29 - Trick or Treat.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Casey Nice, Jolee Love & Lucy Heart - Czech VR - 2020-10-03 - Meet my Horny Friends.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Charlie Red, Anna Rose & Sabrisse Aaliyah - Czech VR - 2021-05-26 - Girl on Girl… on Girl!.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Cindy Shine & Mia Trejsi - Czech VR - 2022-11-05 - Who Looks Better.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Cindy Shine - Czech VR - 2020-08-01 - Ass Pounding.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Claudia Mac - Czech VR - 2021-11-27 - Horny Preggo.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Eva Brown - Czech VR - 2022-01-10 - Fun in Bath.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Frida Sante & Georgie Lyall - Czech VR - 2018-07-07 - Slutty Wife and Sexy Helper.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Georgie Lyall - Czech VR - 2019-05-11 - Sexy Receptionist.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Gina Gerson, Casey Nice & Renata Fox - Czech VR - 2019-05-04 - Russian Foursome.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Holly Molly - Czech VR - 2022-12-12 - Hornier Than Tired.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Jayla De Angelis - Czech VR - 2021-12-18 - Erection Time!.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Jayla De Angelis - Czech VR - 2022-05-28 - Mending Broken Heart.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Jenny Doll & Stacy Cruz - Czech VR - 2020-12-23 - Christmas Wood.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Josephine Jackson - Czech VR - 2020-02-01 - Shower Tease.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Katy Rose & Veronica Leal - Czech VR - 2019-10-19 - Garden Mini-Golf.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Kinuski - Czech VR - 2021-05-15 - So! Many! Orgasms!.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Koko Amaris & Megan Venturi - Czech VR - 2022-07-09 - Let's Take it Inside.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Koko Amaris - Czech VR - 2022-01-08 - Dressed to Play.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Koko Amaris, Tina Kay & Josephine Jackson - Czech VR - 2019-12-07 - Playing it Cool.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lexi Dona - Czech VR - 2021-08-16 - Bathtub Romance.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Linda Sweet - Czech VR - 2019-05-25 - The Escort Girl.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lovita Fate & Sybil - Czech VR - 2021-12-22 - A Sweet Surprise.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lovita Fate, Alexis Crystal, Ashley Ocean & Freya Dee - Czech VR - 2019-08-14 - From Fishing to Heaven 1.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lovita Fate, Alexis Crystal, Ashley Ocean & Freya Dee - Czech VR - 2019-08-17 - From Fishing to Heaven 2.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lovita Fate, Alexis Crystal, Isabela De Laa, Sofia Lee, Jennifer Mendez, Jenny Wild & Lexi Dona - Czech VR - 2021-11-03 - Magnificent Seven Part 1.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lovita Fate, Alexis Crystal, Isabela De Laa, Sofia Lee, Jennifer Mendez, Jenny Wild & Lexi Dona - Czech VR - 2021-11-06 - Magnificent Seven Part 2.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Lucy Heart - Czech VR - 2020-01-29 - Sleeping Beauty.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Olivia Sparkle, Rika Fane, Antonia Sainz & Lucky Bee - Czech VR - 2022-04-16 - Easter Game Part 1.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Olivia Sparkle, Rika Fane, Antonia Sainz & Lucky Bee - Czech VR - 2022-04-18 - Easter Game Part 2.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Rebecca Volpetti - Czech VR - 2022-09-24 - Reward for a Trip.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Stacy Cruz - Czech VR - 2019-02-02 - Professional Slut.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Stacy Cruz - Czech VR - 2020-05-02 - Penthouse Sex.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Stacy Cruz - Czech VR - 2022-08-06 - Feeling Hot.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Sybil - Czech VR - 2020-01-11 - I Love Your Hands.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Sybil, Lexi Dona & Daphne Angel - Czech VR - 2017-05-01 - Dream like never before.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Veronica Leal - Czech VR - 2018-11-24 - Squirting Concubine.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Veronica Leal - Czech VR - 2019-06-01 - Boner for Breakfast.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Veronica Leal - Czech VR - 2020-09-19 - Tending Garden.mp4",
+                        @"I:\Storage Evaluation\Czech VR\Vinna Reed - Czech VR - 2020-06-20 - Horny from Shower.mp4",
+                    };
+                    await networkRipper.UpsizeScenesAsync(site, browserSettings, downloadOptions, fileNames);
+                    break;
+                default:
+                    throw new NotImplementedException($"Unknown job type {jobType}");
+            }
         }
         catch (Exception ex)
         {
