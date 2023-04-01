385327488e7cafa596be2b63929545eb8bddaa3b
Ville Salonen <ville.salonen@iki.fi>
2023-04-01 08:12:11 +0300

    Fix foreign key reference for subsites

---
+++ CultureExtractor/Migrations/20230401044620_SubSiteForeignKeyFix.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CultureExtractor.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubSiteForeignKeyFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Scenes_Sites_SubSiteId",
+                table: "Scenes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Scenes_SubSites_SubSiteId",
+                table: "Scenes",
+                column: "SubSiteId",
+                principalTable: "SubSites",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Scenes_SubSites_SubSiteId",
+                table: "Scenes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Scenes_Sites_SubSiteId",
+                table: "Scenes",
+                column: "SubSiteId",
+                principalTable: "Sites",
+                principalColumn: "Id");
+        }
+    }
+}
