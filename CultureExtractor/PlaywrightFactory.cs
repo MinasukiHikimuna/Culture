a1e5c6c8fc3bcfa61c8125e8529d032a6d025e46
Ville Salonen <ville.salonen@iki.fi>
2023-12-03 14:13:34 +0200

    Retry for PlaywrightFactory if the initial page load fails

--- CultureExtractor/PlaywrightFactory.cs
+++ CultureExtractor/PlaywrightFactory.cs
@@ -1,12 +1,35 @@
 ﻿using CultureExtractor.Interfaces;
 using CultureExtractor.Models;
 using Microsoft.Playwright;
+using Polly;
+using Serilog;
 
 namespace CultureExtractor;
 
 public class PlaywrightFactory : IPlaywrightFactory
 {
     public async Task<IPage> CreatePageAsync(Site site, BrowserSettings browserSettings)
+    {
+        var strategy = new ResiliencePipelineBuilder<IPage>()
+            .AddRetry(new ()
+            {
+                MaxRetryAttempts = 3,
+                Delay = TimeSpan.FromSeconds(10),
+                OnRetry = args =>
+                {
+                    var ex = args.Outcome.Exception;
+                    Log.Error($"Caught following exception while creating page for {site.Url}: " + ex, ex);
+                    return default;
+                }
+            })
+            .Build();
+        
+        return await strategy.ExecuteAsync(async token =>
+            await CreatePageInternalAsync(site, browserSettings, token)
+        );
+    }
+
+    private async Task<IPage> CreatePageInternalAsync(Site site, BrowserSettings browserSettings, CancellationToken token)
     {
         var playwright = await Playwright.CreateAsync();
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
