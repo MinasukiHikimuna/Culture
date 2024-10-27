76e903ad4b961baeeb92bbf6235598fe4ba66e72
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-10-27 14:15:37 +0200

    Major upgrades to gallery dates

--- libraries/StashDbClient.py
+++ libraries/StashDbClient.py
@@ -1,8 +1,21 @@
 import requests
 import stashapi.log as logger
 
-from StashboxClient import StashboxClient
+from abc import ABC, abstractmethod
 
+
+class StashboxClient(ABC):
+    @abstractmethod
+    def query_performer_image(self, performer_stash_id):
+        pass
+
+    @abstractmethod
+    def query_studio_image(self, performer_stash_id):
+        pass
+
+    @abstractmethod
+    def query_scenes(self, performer_stash_id):
+        pass
 
 class StashDbClient(StashboxClient):
     def __init__(self, endpoint, api_key):
