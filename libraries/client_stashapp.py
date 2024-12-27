771b127b23349ab46513a0f6436c2bd053d913bb
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2024-12-27 12:34:59 +0200

    First version of Polars-based StashAppClient

--- libraries/client_stashapp.py
+++ libraries/client_stashapp.py
@@ -1,11 +1,15 @@
+from typing import List
+import numpy as np
 import pandas as pd
 import stashapi.log as log
 from stashapi.stashapp import StashInterface
 from dotenv import load_dotenv
 import os
+import polars as pl
 
 # Load the .env file
 load_dotenv()
+
 
 def get_stashapp_client(prefix=""):
     # Use the provided prefix to get environment variables
@@ -24,3 +28,244 @@ def get_stashapp_client(prefix=""):
         }
     )
     return stash
+
+
+class StashAppClientPolars:
+    def __init__(self, prefix=""):
+        self.stash = get_stashapp_client(prefix)
+
+    def get_studios(self) -> pl.DataFrame:
+        fragment = """
+        id
+        name
+        url
+        parent_studio {
+            id
+            name
+            url
+        }
+        """
+
+        stashapp_studios = self.stash.find_studios(fragment=fragment)
+
+        studios = []
+        for studio in stashapp_studios:
+            studio_data = {
+                "stash_studios_id": int(studio.get("id")),
+                "stash_studios_name": studio.get("name"),
+                "stash_studios_url": studio.get("url"),
+            }
+            parent_studio = studio.get("parent_studio")
+            if parent_studio:
+                studio_data["stash_studios_parent_studio_id"] = int(parent_studio.get("id"))
+                studio_data["stash_studios_parent_studio_name"] = parent_studio.get("name")
+                studio_data["stash_studios_parent_studio_url"] = parent_studio.get("url")
+            else:
+                studio_data["stash_studios_parent_studio_id"] = None
+                studio_data["stash_studios_parent_studio_name"] = None
+                studio_data["stash_studios_parent_studio_url"] = None
+            studios.append(studio_data)
+
+        schema = {
+            "stash_studios_id": pl.Int64,
+            "stash_studios_name": pl.Utf8,
+            "stash_studios_url": pl.Utf8,
+            "stash_studios_parent_studio_id": pl.Int64,
+            "stash_studios_parent_studio_name": pl.Utf8,
+            "stash_studios_parent_studio_url": pl.Utf8,
+        }
+
+        df_studios = pl.DataFrame(studios, schema=schema)
+        return df_studios
+
+    def find_scenes_by_oshash(self, oshashes: List[str]) -> pl.DataFrame:
+        fragment = """
+        id
+        title
+        details
+        date
+        urls
+        created_at
+        updated_at
+        organized
+        interactive
+        play_duration
+        play_count
+        o_counter
+        """
+        
+        todo_fragment = """
+        files {
+            id
+            path
+            size
+            duration
+        }
+        studio {
+            id
+            name
+            parent_studio {
+                id
+                name
+            }
+        }
+        tags {
+            id
+            name
+        }
+        performers {
+            id
+            name
+            disambiguation
+            alias_list
+            gender
+        }
+
+        """
+
+        scenes = []
+        for oshash in oshashes:
+            stash_scene = self.stash.find_scene_by_hash({"oshash": oshash}, fragment)
+            if stash_scene:
+                # Extract fields according to our schema
+                scene_data = {
+                    "id": int(stash_scene.get("id")),
+                    "title": stash_scene.get("title", ""),
+                    "details": stash_scene.get("details", ""),
+                    "date": stash_scene.get("date", ""),
+                    "urls": stash_scene.get("urls", []),
+                    "created_at": stash_scene.get("created_at", ""),
+                    "updated_at": stash_scene.get("updated_at", ""),
+                    # "files": stash_scene.get("files", []),
+                    # "studio": stash_scene.get("studio", {}),
+                    # "tags": stash_scene.get("tags", []),
+                    # "performers": stash_scene.get("performers", []),
+                    "organized": stash_scene.get("organized", False),
+                    "interactive": stash_scene.get("interactive", False),
+                    "play_duration": stash_scene.get("play_duration", 0),
+                    "play_count": stash_scene.get("play_count", 0),
+                    "o_counter": stash_scene.get("o_counter", 0),
+                }
+                scenes.append(scene_data)
+
+        schema = {
+            "id": pl.Int64,
+            "title": pl.Utf8,
+            "details": pl.Utf8,
+            "date": pl.Utf8,
+            "urls": pl.List(pl.Utf8),
+            "created_at": pl.Utf8,
+            "updated_at": pl.Utf8,
+            #"files": pl.List(pl.Struct),
+            #"studio": pl.Struct,
+            #"tags": pl.List(pl.Struct),
+            #"performers": pl.List(pl.Struct),
+            "organized": pl.Boolean,
+            "interactive": pl.Boolean,
+            "play_duration": pl.Int64,
+            "play_count": pl.Int64,
+            "o_counter": pl.Int64,
+        }
+
+        df_scenes = pl.DataFrame(scenes, schema=schema)
+        return df_scenes
+
+
+
+class StashAppClient:
+    def __init__(self, prefix=""):
+        self.stash = get_stashapp_client(prefix)
+
+    def get_studios(self) -> pd.DataFrame:
+        stashapp_studios = self.stash.find_studios()
+
+        studios = []
+        for studio in stashapp_studios:
+            studio_data = {
+                "id": int(studio.get("id")),
+                "name": studio.get("name"),
+                "url": studio.get("url"),
+            }
+            parent_studio = studio.get("parent_studio")
+            if parent_studio:
+                studio_data["parent_studio_id"] = int(parent_studio.get("id"))
+            #     studio_data["parent_studio_name"] = parent_studio.get("name")
+            #     studio_data["parent_studio_url"] = parent_studio.get("url")
+            else:
+                studio_data["parent_studio_id"] = np.nan
+            #     studio_data["parent_studio_name"] = None
+            #     studio_data["parent_studio_url"] = None
+            studios.append(studio_data)
+
+        df_studios = pd.DataFrame(studios)
+        df_studios = df_studios.add_prefix("stash_studios_")
+        return df_studios
+
+    def find_scenes_by_oshash(self, oshashes: List[str]) -> pd.DataFrame:
+        fragment = """
+        id
+        title
+        details
+        date
+        urls
+        created_at
+        updated_at
+        files {
+            id
+            path
+            size
+            duration
+        }
+        studio {
+            id
+            name
+            parent_studio {
+                id
+                name
+            }
+        }
+        tags {
+            id
+            name
+        }
+        performers {
+            id
+            name
+            disambiguation
+            alias_list
+            gender
+        }
+        organized
+        interactive
+        play_duration
+        play_count
+        o_counter
+        """
+
+        scenes = []
+        for oshash in oshashes:
+            stash_scene = self.stash.find_scene_by_hash({"oshash": oshash}, fragment)
+            if stash_scene:
+                # Extract fields according to our schema
+                scene_data = {
+                    "id": int(stash_scene.get("id")),
+                    "title": stash_scene.get("title", ""),
+                    "details": stash_scene.get("details", ""),
+                    "date": stash_scene.get("date", ""),
+                    "urls": stash_scene.get("urls", []),
+                    "created_at": stash_scene.get("created_at", ""),
+                    "updated_at": stash_scene.get("updated_at", ""),
+                    "files": stash_scene.get("files", []),
+                    "studio": stash_scene.get("studio", {}),
+                    "tags": stash_scene.get("tags", []),
+                    "performers": stash_scene.get("performers", []),
+                    "organized": stash_scene.get("organized", False),
+                    "interactive": stash_scene.get("interactive", False),
+                    "play_duration": stash_scene.get("play_duration", 0),
+                    "play_count": stash_scene.get("play_count", 0),
+                    "o_counter": stash_scene.get("o_counter", 0),
+                }
+                scenes.append(scene_data)
+
+        df_scenes = pd.DataFrame(scenes)
+        return df_scenes
