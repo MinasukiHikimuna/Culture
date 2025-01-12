e919b12804b069392099a25653cc6ada6ed9a040
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2025-01-12 17:13:29 +0200

    Update user agent

--- scrapy/scrapy/cultureextractorscrapy/settings.py
+++ scrapy/scrapy/cultureextractorscrapy/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "cultureextractorscrapy.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
