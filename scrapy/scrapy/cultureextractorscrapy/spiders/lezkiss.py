5f6560cabea1030afb1ada80dbafd9780c3e2983
Minasuki Hikimuna <minasuki.hikimuna@chiefsclub.com>
2025-03-11 04:31:09 +0200

    Very initial LezKiss file structure

---
+++ scrapy/scrapy/cultureextractorscrapy/spiders/lezkiss.py
@@ -0,0 +1,64 @@
+import os
+import json
+import newnewid
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+import scrapy
+from cultureextractorscrapy.spiders.database import (
+    get_site_item,
+    get_or_create_performer,
+    get_or_create_tag,
+    get_existing_releases_with_status,
+)
+from cultureextractorscrapy.items import (
+    AvailableGalleryZipFile,
+    AvailableImageFile,
+    AvailableVideoFile,
+    AvailableFileEncoder,
+    ReleaseItem,
+    DirectDownloadItem,
+)
+from cultureextractorscrapy.utils import (
+    parse_resolution_height,
+    parse_resolution_width,
+    get_log_filename,
+)
+from itemadapter import ItemAdapter
+
+load_dotenv()
+
+cookies = json.loads(os.getenv("LEZKISS_COOKIES"))
+base_url = os.getenv("LEZKISS_BASE_URL")
+
+
+class LezKissSpider(scrapy.Spider):
+    name = "lezkiss"
+    allowed_domains = os.getenv("LEZKISS_ALLOWED_DOMAINS").split(",")
+    start_urls = [base_url]
+    site_short_name = "lezkiss"
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(LezKissSpider, cls).from_crawler(crawler, *args, **kwargs)
+
+        # Set the log file using the spider name
+        crawler.settings.set("LOG_FILE", get_log_filename(spider.name))
+
+        # Get force_update from crawler settings or default to False
+        spider.force_update = crawler.settings.getbool("FORCE_UPDATE", False)
+
+        site_item = get_site_item(spider.site_short_name)
+        if site_item is None:
+            raise ValueError(
+                f"Site with short_name '{spider.site_short_name}' not found in the database."
+            )
+        spider.site = site_item
+
+        # Get existing releases with their download status
+        spider.existing_releases = get_existing_releases_with_status(site_item.id)
+        return spider
+
+    def parse(self, response):
+        """Initial parse method - entry point for the spider."""
+        # This will be implemented based on the site structure
+        pass
